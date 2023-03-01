ef2b09301cc80803e8ec5cabdfe7f9b4f533b5df
NganPham89 <algarvian2001@yahoo.com>
2023-03-01 00:33:50 -0500

    added weather display functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,12 @@
-let requestWeatherURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=2f86475148abed664b43332f701d4d90";
+// let requestWeatherURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=2f86475148abed664b43332f701d4d90";
 
-let requestGeoURL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},US&limit=5&appid=2f86475148abed664b43332f701d4d90";
-
-let statesArray =  ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
 let citiesArray = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alexandria", "Allentown", "Amarillo", "Anaheim", "Anchorage", "Ann Arbor", "Antioch", "Apple Valley", "Appleton", "Arlington", "Arvada", "Asheville", "Athens", "Atlanta", "Atlantic City", "Augusta", "Aurora", "Austin", "Bakersfield", "Baltimore", "Barnstable", "Baton Rouge", "Beaumont", "Bel Air", "Bellevue", "Berkeley", "Bethlehem", "Billings", "Birmingham", "Bloomington", "Boise", "Boise City", "Bonita Springs", "Boston", "Boulder", "Bradenton", "Bremerton", "Bridgeport", "Brighton", "Brownsville", "Bryan", "Buffalo", "Burbank", "Burlington", "Cambridge", "Canton", "Cape Coral", "Carrollton", "Cary", "Cathedral City", "Cedar Rapids", "Champaign", "Chandler", "Charleston", "Charlotte", "Chattanooga", "Chesapeake", "Chicago", "Chula Vista", "Cincinnati", "Clarke County", "Clarksville", "Clearwater", "Cleveland", "College Station", "Colorado Springs", "Columbia", "Columbus", "Concord", "Coral Springs", "Corona", "Corpus Christi", "Costa Mesa", "Dallas", "Daly City", "Danbury", "Davenport", "Davidson County", "Dayton", "Daytona Beach", "Deltona", "Denton", "Denver", "Des Moines", "Detroit", "Downey", "Duluth", "Durham", "El Monte", "El Paso", "Elizabeth", "Elk Grove", "Elkhart", "Erie", "Escondido", "Eugene", "Evansville", "Fairfield", "Fargo", "Fayetteville", "Fitchburg", "Flint", "Fontana", "Fort Collins", "Fort Lauderdale", "Fort Smith", "Fort Walton Beach", "Fort Wayne", "Fort Worth", "Frederick", "Fremont", "Fresno", "Fullerton", "Gainesville", "Garden Grove", "Garland", "Gastonia", "Gilbert", "Glendale", "Grand Prairie", "Grand Rapids", "Grayslake", "Green Bay", "GreenBay", "Greensboro", "Greenville", "Gulfport-Biloxi", "Hagerstown", "Hampton", "Harlingen", "Harrisburg", "Hartford", "Havre de Grace", "Hayward", "Hemet", "Henderson", "Hesperia", "Hialeah", "Hickory", "High Point", "Hollywood", "Honolulu", "Houma", "Houston", "Howell", "Huntington", "Huntington Beach", "Huntsville", "Independence", "Indianapolis", "Inglewood", "Irvine", "Irving", "Jackson", "Jacksonville", "Jefferson", "Jersey City", "Johnson City", "Joliet", "Kailua", "Kalamazoo", "Kaneohe", "Kansas City", "Kennewick", "Kenosha", "Killeen", "Kissimmee", "Knoxville", "Lacey", "Lafayette", "Lake Charles", "Lakeland", "Lakewood", "Lancaster", "Lansing", "Laredo", "Las Cruces", "Las Vegas", "Layton", "Leominster", "Lewisville", "Lexington", "Lincoln", "Little Rock", "Long Beach", "Lorain", "Los Angeles", "Louisville", "Lowell", "Lubbock", "Macon", "Madison", "Manchester", "Marina", "Marysville", "McAllen", "McHenry", "Medford", "Melbourne", "Memphis", "Merced", "Mesa", "Mesquite", "Miami", "Milwaukee", "Minneapolis", "Miramar", "Mission Viejo", "Mobile", "Modesto", "Monroe", "Monterey", "Montgomery", "Moreno Valley", "Murfreesboro", "Murrieta", "Muskegon", "Myrtle Beach", "Naperville", "Naples", "Nashua", "Nashville", "New Bedford", "New Haven", "New London", "New Orleans", "New York", "New York City", "Newark", "Newburgh", "Newport News", "Norfolk", "Normal", "Norman", "North Charleston", "North Las Vegas", "North Port", "Norwalk", "Norwich", "Oakland", "Ocala", "Oceanside", "Odessa", "Ogden", "Oklahoma City", "Olathe", "Olympia", "Omaha", "Ontario", "Orange", "Orem", "Orlando", "Overland Park", "Oxnard", "Palm Bay", "Palm Springs", "Palmdale", "Panama City", "Pasadena", "Paterson", "Pembroke Pines", "Pensacola", "Peoria", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", "Pomona", "Pompano Beach", "Port Arthur", "Port Orange", "Port Saint Lucie", "Port St. Lucie", "Portland", "Portsmouth", "Poughkeepsie", "Providence", "Provo", "Pueblo", "Punta Gorda", "Racine", "Raleigh", "Rancho Cucamonga", "Reading", "Redding", "Reno", "Richland", "Richmond", "Richmond County", "Riverside", "Roanoke", "Rochester", "Rockford", "Roseville", "Round Lake Beach", "Sacramento", "Saginaw", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salem", "Salinas", "Salt Lake City", "San Antonio", "San Bernardino", "San Buenaventura", "San Diego", "San Francisco", "San Jose", "Santa Ana", "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Maria", "Santa Rosa", "Sarasota", "Savannah", "Scottsdale", "Scranton", "Seaside", "Seattle", "Sebastian", "Shreveport", "Simi Valley", "Sioux City", "Sioux Falls", "South Bend", "South Lyon", "Spartanburg", "Spokane", "Springdale", "Springfield", "St. Louis", "St. Paul", "St. Petersburg", "Stamford", "Sterling Heights", "Stockton", "Sunnyvale", "Syracuse", "Tacoma", "Tallahassee", "Tampa", "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"];
 
-let stateMenu = $(".stateMenu");
-
 let cityInput = $(".cityInput").val();
-let stateInput = $(".stateInput").val();
+
+let searchButton = $(".searchButton");
+
+let requestCityWeather = "http://api.openweathermap.org/data/2.5/forecast?q="+cityInput+"&appid=2f86475148abed664b43332f701d4d90";
 
 $(function() {
     $("#majorCities").autocomplete({
@@ -16,12 +14,45 @@ $(function() {
     });
   } );
 
-  $(function() {
-    $("#usStates").autocomplete({
-      source: statesArray
-    });
-  } );
+async function findCurrentWeatherInfo() {
+    let cityInput = $(".cityInput").val();
+    let requestCityWeather = "http://api.openweathermap.org/data/2.5/forecast?q="+cityInput+"&units=imperial&appid=2f86475148abed664b43332f701d4d90";
+    let response = await fetch(requestCityWeather);
+    if (response.status !== 200) {
+        alert("No city found.")
+    }
+    let weatherData = await response.json(); 
 
-  function findGeoInfo() {
-    
+    generateTodayForecast(weatherData);
+    generateFiveForecasts(weatherData);
   }
+
+let foreCastContainer = $(".foreCastContainer");
+
+function generateTodayForecast(weatherData) {
+
+    let currentCity = weatherData.city.name;
+    let currentTime = dayjs.unix(weatherData.list[0].dt).format('(MMMM, DD YYYY)');
+    let currentIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + weatherData.list[0].weather[0].icon + "@2x.png");
+    let cityTimeDisplay = $("<h2>").text(currentCity + " " + currentTime).append(currentIcon);
+
+    let currentTemp = $("<h3>").text("Temp: " + weatherData.list[0].main.temp + " \u2109");
+    let currentWind = $("<h3>").text("Wind: " + weatherData.list[0].wind.speed + " MPH");
+    let currentHumidity = $("<h3>").text("Humidity: " + weatherData.list[0].main.humidity + "%");
+    $(".cityWeatherInfo").append(cityTimeDisplay, currentTemp, currentWind, currentHumidity);
+
+}
+
+function generateFiveForecasts(weatherData) {
+    for (var i = 1; i < 6; i++) {
+        let singleForecastContainer = $("<div>").addClass("singleForecastContainer");
+        let eachUnixTime = weatherData.list[i].dt;
+        console.log(eachUnixTime)
+        let eachForecastTime = dayjs.unix(eachUnixTime).format('(MM/DD/YYYY)');
+        console.log(eachForecastTime);
+        let eachTimeDisplay = $("<h2>").text();
+        $(foreCastContainer).append(singleForecastContainer.append(eachTimeDisplay));
+    }
+}
+
+$(searchButton).click(findCurrentWeatherInfo);
